378429ba646115d33f407ebadd363df9e210f7d2
dionysen <solongnight@outlook.com>
2023-08-21 21:32:13 +0800

    update

--- Template/Old/GettingStarted/xmake.lua
+++
@@ -1,9 +0,0 @@
-add_rules("mode.debug", "mode.release")
-add_requires("glm")
-
-target("opengl")
-    set_kind("binary")
-    add_files("src/*.cpp","src/*.c")
-    add_includedirs("include", "Dependencies/include/glad", "Dependencies/include","shaders")
-    add_links("glfw")
-    add_packages("glm")
